d59be174a66d7cc74086fa166874993e6c224c15
Pierre Mesure <pierre.mesure@gmail.com>
2018-02-05 18:43:04 +0100

    Disabling asset initialisation on precompile

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,9 @@ module Consul
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
+    # Test
+    config.assets.initialize_on_precompile = false
+    
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
