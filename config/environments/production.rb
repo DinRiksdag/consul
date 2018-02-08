c3b0dd345b2cffd78b4e17373e16467c86dc4950
Pierre Mesure <pierre.mesure@gmail.com>
2018-02-08 17:19:19 +0100

    Trying to remove domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,6 @@ Rails.application.configure do
     config.action_mailer.smtp_settings = {
         :address              => ENV['MAILGUN_SMTP_SERVER'],
         :port                 => ENV['MAILGUN_SMTP_PORT'],
-        :domain               => 'mg.dinriksdag.se',
         :user_name            => ENV['MAILGUN_SMTP_LOGIN'],
         :password             => ENV['MAILGUN_SMTP_PASSWORD'],
         :authentication => :plain,
