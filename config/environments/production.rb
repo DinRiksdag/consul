910e2cef93b2bdfc2de3f09049ad9be2ceb2c4c7
Pierre Mesure <pierre.mesure@gmail.com>
2018-02-08 16:29:55 +0100

    Trying with SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
         :password             => ENV['MAILGUN_SMTP_PASSWORD'],
         :authentication => :plain,
         :enable_starttls_auto => true,
-        :ssl => false
+        :ssl => true
     }
 
 end
